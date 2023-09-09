45b839a5c2fa8727e5f0aab25ce8630ff5e600b8
Kris Salmi <kristiina.k.salmi@gmail.com>
2023-09-09 23:36:53 +0300

    Small change to displaying confidence

--- src/components/Modals/PredictionModal.jsx
+++ src/components/Modals/PredictionModal.jsx
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types';
 const ResultModal = ({show, onClose, prediction, confidence}) => {
 
     const buttonClasses = `btn btn-primary ${classes.modalCloseButton}`
+    const confidenceEstimate = confidence === "100.00" ? "> 99.99" : confidence;
   
     return (
         <Modal
@@ -19,7 +20,7 @@ const ResultModal = ({show, onClose, prediction, confidence}) => {
            <div>
                 <div className={classes.modalContent}>
                     <h3>Analysis complete!</h3>
-                    <h5>The model predicts the drawing to be a <span className={classes.bold}>{prediction}</span> with <span className={classes.bold}>{confidence}%</span> confidence.</h5>
+                    <h5>The model predicts the drawing to be a <span className={classes.bold}>{prediction}</span> with <span className={classes.bold}>{confidenceEstimate}%</span> confidence.</h5>
                 </div>
                 <div className={classes.btnDiv}>
                     <button className={buttonClasses} onClick={onClose}>OK</button>
