6ccf1e5590410297ffd340f3e5c78fd8c390c31d
Kris Salmi <kristiina.k.salmi@gmail.com>
2024-03-08 13:31:06 -0500

    fix: Grew trainingLines height and updated year

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className={classes.footer}>
             <div className={classes.footerText}>
-                <p>© 2023 <a href="https://www.linkedin.com/in/kristiina-salmi-3a5549194/">Kristiina Salmi</a>. Check out the full code on my <a href="https://github.com/krsalmi/ai_digits_frontend">Github</a></p>
+                <p>© 2024 <a href="https://www.linkedin.com/in/kristiina-salmi-3a5549194/">Kristiina Salmi</a>. Check out the full code on my <a href="https://github.com/krsalmi/ai_digits_frontend">Github</a></p>
             </div>
         </footer>
     );
