949b898ef028097246008bcde4430af2a4c6c064
Kris Salmi <kristiina.k.salmi@gmail.com>
2024-03-09 15:04:24 -0500

    test: Fade in load, resizing imgDiv

--- src/components/ImageWithConditionalFadeIn/ImageWithConditionalFadeIn.jsx
+++ src/components/ImageWithConditionalFadeIn/ImageWithConditionalFadeIn.jsx
@@ -1,33 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import classes from './ImageWithConditionalFadeIn.module.css';
 
-const ImageWithConditionalFadeIn = ({ src, alt, className = '' }) => {
-  const [useFadeIn, setUseFadeIn] = useState(false);
+const ImageWithConditionalFadeIn = ({ src, alt, className }) => {
+  const [loaded, setLoaded] = useState(false);
 
-  useEffect(() => {
-    const img = new Image();
-    const start = performance.now();
 
-    img.onload = () => {
-      const end = performance.now();
-      // Determine if we need the fade-in effect based on loading time
-      setUseFadeIn(end - start > 50);
-    };
+  // Apply the 'imageLoaded' class when the image is loaded for a fade-in effect
+  // The external className is appended directly
+  const imageClass = `${loaded ? classes.imageLoaded : classes.imageInitial} ${classes[className]}`;
 
-    img.src = src;
-    if (img.complete) {
-      // If image is cached and loads immediately, we might skip fade-in
-      setUseFadeIn(false);
-    }
-  }, [src]);
 
-  // Combine the dynamic class name based on `useFadeIn`
-  const imageClass = `${classes[className]} ${
-    useFadeIn ? classes.imageInitial : `${classes.imageLoaded} ${classes.imageNoTransition}`
-  }`;
-
-  return <img src={src} alt={alt} className={imageClass} onLoad={() => setUseFadeIn(false)} />;
+return (
+    <img
+      src={src}
+      alt={alt}
+      className={imageClass}
+      onLoad={() => setLoaded(true)}
+    />
+  );
 };
-
 
 export default ImageWithConditionalFadeIn;
